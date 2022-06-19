6c5751c1df5d252dafd54ba1b2f83aefdd3bc01e
BinoManjesh <binomanjesh@gmail.com>
2022-06-19 16:21:19 +0530

    Add function playRound

--- script.js
+++ script.js
@@ -9,3 +9,21 @@ function computerPlay() {
             return "Scissors";
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    let capitalize = str => str.slice(0, 1).toUpperCase() + str.slice(1).toLowerCase();
+    playerSelection = capitalize(playerSelection);
+    computerSelection = capitalize(computerSelection);
+    if (playerSelection === computerSelection) {
+        return `It's a Tie! ${playerSelection} ties ${computerSelection}`
+    }
+    let playerWon = 
+        (playerSelection === "Rock" && computerSelection === "Scissors") ||
+        (playerSelection === "Paper" && computerSelection === "Rock") ||
+        (playerSelection === "Scissors" && computerSelection === "Paper");
+    if (playerWon) {
+        return `You Won! ${playerSelection} beats ${computerSelection}`;
+    } else {
+        return `You Lost! ${computerSelection} beats ${playerSelection}`;
+    }
+}
